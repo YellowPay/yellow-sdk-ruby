02893c7e7013cd2b847486d1f22e5482357bba7c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-04-29 15:51:51 +0300

    Ruby SDK v0.0.1

--- lib/yellow-sdk/client.rb
+++ lib/yellow-sdk/client.rb
@@ -1,6 +1,6 @@
 module Yellow
 
-        VERSION = "0.1"
+        VERSION = "0.0.1"
         YELLOW_SERVER = "https://" + (ENV["YELLOW_SERVER"] || "api.yellowpay.co")
 
     class YellowApiError < Exception ; end
@@ -11,49 +11,56 @@ module Yellow
             @api_secret = api_secret
         end
 
-        def create_invoice(base_ccy, base_price, callback)
+        def create_invoice(hash={})
             ####
-            # This method creates a yellow invoices based on the bellow parameters:
+            # This method creates a yellow invoices based on the following options:
             #
-            # #param    str    base_ccy      required - a 3-letter currency code
-            #                                (e.g. “AED”) representing the national 
-            #                                currency used by the merchant.
-            # #param    str    base_price    required - The invoice price in the 
-            #                                national currency (e.g. “20”).
-            # #param    str    callback      required - The callback URL we'll POST
-            #                                payment notifications to.
+            # #param    hash    hash      hash keys matching the expected
+            #                             HTTP arguments described here:
+            #                             http://yellowpay.co/docs/api/#creating-invoices
             # #returns  JSON parsed data
             ####
 
             endpoint = "/v1/invoice/"
 
-            body = { 
-                        :base_ccy => base_ccy,
-                        :base_price => base_price,
-                        :callback => callback
-                    }.to_json
-
-            make_request('Post', endpoint, body)
+            make_request('POST', endpoint, hash.to_json)
         end
 
         def query_invoice(invoice_id)
             ####
             # Use this method to query an invoice you created earlier using its ID 
             #
-            # #param    str    invoice_id    required - the ID of the invoice you're querying
+            # #param    str    invoice_id    the ID of the invoice you're querying
             # 
             # #returns  JSON parsed data                                
             ####
 
-            endpoint = "/v1/invoice/#{invoice_id}"
-            make_request('Get', endpoint, "")
+            endpoint = "/v1/invoice/#{invoice_id}/"
+            make_request('GET', endpoint, "")
         end
 
-        def verify_ipn
+        def verify_ipn(host_url, request_signature, request_nonce, request_body)
+            ####
+            # This is a helper method to verify that the request you just received really is from Yellow:
+            #
+            # #param    str    host_url             the callback URL you set when you created the invoice
+            # #param    str    request_signature    the signature header of the request
+            # #param    str    request_nonce        the nonce header of the request
+            # #param    str    request_body         the body of the request
+            #
+            # #returns  boolean
+            ####
+
+            signature = get_signature(host_url, request_body, request_nonce, @api_secret)
+            signature == request_signature ? return true : return false
         end
 
         private
         def make_request(method, endpoint, body)
+            ####
+            # A method used by our SDK to make requests to our server.
+            ####
+
             url = YELLOW_SERVER + endpoint
 
             nonce = Time.now.to_i * 1000
@@ -62,11 +69,10 @@ module Yellow
             uri = URI.parse(YELLOW_SERVER)
             http = Net::HTTP.new(uri.host, uri.port)
             http.use_ssl = true
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-            if method == 'Get'
+            if method == 'GET'
                 request = Net::HTTP::Get.new(endpoint)
-            elsif method == 'Post'
+            elsif method == 'POST'
                 request = Net::HTTP::Post.new(endpoint)
             end
             
@@ -74,6 +80,8 @@ module Yellow
             request.add_field('API-Key', @api_key)
             request.add_field('API-Nonce', nonce)
             request.add_field('API-Sign', signature)
+            request.add_field('API-Platform', "#{RUBY_PLATFORM} - Ruby #{RUBY_VERSION}")
+            request.add_field('API-Version', VERSION)
             request.body = body
             response = http.request(request)
             
@@ -87,8 +95,9 @@ module Yellow
         private
         def get_signature(url, body, nonce, api_secret)
             ####
-            # A tiny function used by our SDK to sign and verify requests.
+            # A tiny method used by our SDK to sign and verify requests.
             ####
+
             message = nonce.to_s + url + body
             hash    = OpenSSL::HMAC.hexdigest('sha256', api_secret, message)
         end
